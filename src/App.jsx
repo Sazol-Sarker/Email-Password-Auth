c7ecf76ccba5a4a6f1eaa411998c9b546cdf0af5
Sazol Sarker <sazolsarker1@gmail.com>
2025-02-09 16:31:29 +0600

    Initial setup with router,tailwindCss,daisy ui and sign up form customised from daisy ui

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     
+      <h1>Email Password login</h1>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <div className="avatar">
+  <div className="w-32 rounded">
+    <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+  </div>
+</div>
+<div className="avatar">
+  <div className="w-20 rounded">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+      alt="Tailwind-CSS-Avatar-component" />
+  </div>
+</div>
+<div className="avatar">
+  <div className="w-16 rounded">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+      alt="Tailwind-CSS-Avatar-component" />
+  </div>
+</div>
+<div className="avatar">
+  <div className="w-8 rounded">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+      alt="Tailwind-CSS-Avatar-component" />
+  </div>
+</div>
+      
+      
+    </>
+  )
+}
+
+export default App
